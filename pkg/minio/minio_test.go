1f24944ca7b90eb18b3c92c11aa62c45173432f2
Peng Junzhi <201250214@smail.nju.edu.cn>
2023-01-28 17:25:55 +0800

    test: add delete minio bucket test

--- pkg/minio/minio_test.go
+++ pkg/minio/minio_test.go
@@ -7,14 +7,18 @@ import (
 	"testing"
 )
 
+const (
+	TestBucketName = "tiktoktest"
+)
+
 // MinioServerUrl 不能包含 '/' 等特殊字符
 // bucket name 只能用小写字母
 func TestCreateBucket(t *testing.T) {
-	CreateBucket("tiktoktest")
+	CreateBucket(TestBucketName)
 }
 
 func TestUploadLocalFile(t *testing.T) {
-	info, err := UploadLocalFile("tiktoktest", "test.mp4", "./test.mp4", "video/mp4")
+	info, err := UploadLocalFile(TestBucketName, "test.mp4", "./test.mp4", "video/mp4")
 	fmt.Println(info, err)
 }
 
@@ -22,12 +26,17 @@ func TestUploadFile(t *testing.T) {
 	file, _ := os.Open("./test.mp4")
 	defer file.Close()
 	fi, _ := os.Stat("./test.mp4")
-	err := UploadFile("tiktoktest", "ceshi2", file, fi.Size())
+	err := UploadFile(TestBucketName, "testing.mp4", file, fi.Size())
 	fmt.Println(err)
 }
 
 func TestGetFileUrl(t *testing.T) {
-	url, err := GetFileUrl("tiktoktest", "test.mp4", 0)
+	url, err := GetFileUrl(TestBucketName, "test.mp4", 0)
 	fmt.Println(url, err, strings.Split(url.String(), "?")[0])
 	fmt.Println(url.Path, url.RawPath)
+}
+
+func TestRemoveOneFile(t *testing.T) {
+	err := RemoveOneFile(TestBucketName, "ceshi2")
+	fmt.Println(err)
 }
