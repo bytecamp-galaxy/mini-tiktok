58ddf070b50e40284d40fae80d4c4aab953d725b
Peng Junzhi <201250214@smail.nju.edu.cn>
2023-01-28 17:22:35 +0800

    fix: remove duplicate

--- feed-server/handler.go
+++ feed-server/handler.go
@@ -4,16 +4,13 @@ import (
 	"context"
 	feed "github.com/bytecamp-galaxy/mini-tiktok/feed-server/kitex_gen/feed"
 	"github.com/bytecamp-galaxy/mini-tiktok/feed-server/kitex_gen/user"
+	"github.com/bytecamp-galaxy/mini-tiktok/pkg/constants"
 	"github.com/bytecamp-galaxy/mini-tiktok/pkg/dal/query"
 	"time"
 )
 
 // FeedServiceImpl implements the last service interface defined in the IDL.
 type FeedServiceImpl struct{}
-
-const (
-	LIMIT = 30 // 单次返回最大视频数
-)
 
 // GetFeed implements the FeedServiceImpl interface. get 30 latest videos with db
 func (s *FeedServiceImpl) GetFeed(ctx context.Context, req *feed.FeedRequest) (resp *feed.FeedResponse, err error) {
@@ -30,7 +27,7 @@ func (s *FeedServiceImpl) GetFeed(ctx context.Context, req *feed.FeedRequest) (r
 	v := q.Video
 
 	// find latest 30 videos
-	videos, err := v.WithContext(ctx).Limit(LIMIT).Order(v.CreatedAt.Desc()).Where(v.CreatedAt.Lt(latestTime)).Find()
+	videos, err := v.WithContext(ctx).Limit(constants.DefaultVideoLimit).Order(v.CreatedAt.Desc()).Where(v.CreatedAt.Lt(latestTime)).Find()
 
 	if err != nil {
 		return nil, err
--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -1,16 +1,14 @@
 package constants
 
 const (
-	VideoTableName  = "video"
-	MinioAccessKey  = "HD5sotcOvhzVV3p8"
-	MinioSecretKey  = "Aq8TOqRL5GS2iMckVfm1FVjtAqBf8Gv4"
-	MinioServerUrl  = "81.68.219.146:9000"
-	MinioApi        = "s3v4"
-	MinioUseSSL     = false
-	EurekaServerUrl = "http://81.68.219.146:8761/eureka"
-	VideoBucketName = "video"
-	ImgBucketName   = "img"
-	MySQLDefaultDSN = "root:123456@tcp(81.68.219.146:3305)/mini_tiktok?charset=utf8&parseTime=True&loc=Local"
-	CPURateLimit    = 80.0
-	DefaultLimit    = 10
+	VideoTableName    = "video"
+	MinioAccessKey    = "HD5sotcOvhzVV3p8"
+	MinioSecretKey    = "Aq8TOqRL5GS2iMckVfm1FVjtAqBf8Gv4"
+	MinioServerUrl    = "81.68.219.146:9000"
+	MinioUseSSL       = false
+	EurekaServerUrl   = "http://81.68.219.146:8761/eureka"
+	VideoBucketName   = "video"
+	ImgBucketName     = "img"
+	MySQLDefaultDSN   = "root:123456@tcp(81.68.219.146:3305)/mini_tiktok?charset=utf8&parseTime=True&loc=Local"
+	DefaultVideoLimit = 30
 )
