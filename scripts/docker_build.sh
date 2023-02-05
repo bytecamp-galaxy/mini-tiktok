c717cfaf379bf005d38d58bbc30e0d4a11e3da9a
张岳轩 <201250179@smail.nju.edu.cn>
2023-02-05 23:49:23 +0800

    chore: add Dockerfile

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
-docker build -f docker/api-server/Dockerfile -t vgalaxy/api-server .
-docker build -f docker/user-server/Dockerfile -t vgalaxy/user-server .
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+build_func() {
+  echo 'Building '$1'-server...'
+  docker build -f docker/$1-server/Dockerfile -t vgalaxy/$1-server . > /dev/null 2>& 1
+  if [ $? == 0 ]
+  then
+    echo -e "${GREEN}Success${NC}"
+  else
+    echo -e "${RED}failed${NC}"
+  fi
+}
+
+servers=('api' 'user' 'comment' 'publish' 'feed')
+
+for server in ${servers[*]}
+do
+  build_func $server
+done
